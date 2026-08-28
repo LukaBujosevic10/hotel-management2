3abbe047c2165063a06901ef9be2b25978263544
Luka Bujosevic <lukabujosevic@gmail.com>
2026-08-28 16:36:58 +0200

    Final

--- guest-service/src/main/java/com/hotel/guest/SwaggerConfig.java
+++ guest-service/src/main/java/com/hotel/guest/SwaggerConfig.java
@@ -1,35 +1,32 @@
 package com.hotel.guest;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import io.swagger.v3.oas.annotations.servers.Server;
-import io.swagger.v3.oas.models.Components;
-import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.security.SecurityRequirement;
-import io.swagger.v3.oas.models.security.SecurityScheme;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@OpenAPIDefinition(servers = { @Server(url = "/", description = "Lokalni server") })
+@OpenAPIDefinition(
+        info = @Info(
+                title = "API Dokumentacija",
+                version = "1.0"
+        ),
+        servers = {
+                @Server(url = "/", description = "Lokalni server")
+        },
+        security = {
+                @SecurityRequirement(name = "bearerAuth")
+        }
+)
+@SecurityScheme(
+        name = "bearerAuth",
+        type = SecuritySchemeType.HTTP,
+        scheme = "bearer",
+        bearerFormat = "JWT"
+)
 public class SwaggerConfig {
-    @Bean
-    public OpenAPI customOpenAPI() {
-        final String securitySchemeName = "bearerAuth";
-
-        return new OpenAPI()
-                // Forsira relativnu putanju (rešava tvoj prethodni Docker/Gateway problem)
-                .servers(java.util.List.of(new io.swagger.v3.oas.models.servers.Server().url("/")))
-                // 1. Dodaje globalni zahtev za sigurnošću na sve API rute
-                .addSecurityItem(new SecurityRequirement().addList(securitySchemeName))
-                // 2. Definiše samu šemu (tip autentifikacije)
-                .components(new Components()
-                        .addSecuritySchemes(securitySchemeName,
-                                new SecurityScheme()
-                                        .name(securitySchemeName)
-                                        .type(SecurityScheme.Type.HTTP)
-                                        .scheme("bearer")
-                                        .bearerFormat("JWT")))
-                .info(new Info().title("API Dokumentacija").version("1.0"));
-    }
+    // Klasa ostaje prazna, sva konfiguracija se radi preko anotacija
 }
